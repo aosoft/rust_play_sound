d256ef09b0cae6167d4fbb64f2055a077131a5e6
aosoft <tan-y@aosoft.jp>
2020-01-18 01:06:00 +0900

    Fix parameter

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use portaudio::{PortAudio, Error};
 use rust_play_sound::{ Note, NoteName, Wave, Time };
 
-const SAMPLE_RATE: f32 = 48000.0;
+const SAMPLE_RATE: u32 = 48000;
 
 fn main() {
     match main2() {
@@ -12,14 +12,14 @@ fn main() {
 
 fn main2() -> Result<(), Error> {
     let pa = PortAudio::new()?;
-    let mut settings = pa.default_output_stream_settings(2, SAMPLE_RATE as f64, 48000)?;
+    let mut settings = pa.default_output_stream_settings(2, SAMPLE_RATE as f64, SAMPLE_RATE)?;
     settings.flags = portaudio::stream_flags::CLIP_OFF;
 
     let mut sample_number:i32 = 0;
     let callback = move |args: portaudio::OutputStreamCallbackArgs<f32>| {
         let mut i = 0;
         for _ in 0..args.frames {
-            let time = Time::samples_to_time(SAMPLE_RATE,sample_number);
+            let time = Time::samples_to_time(SAMPLE_RATE as f32,sample_number);
             let value =
                 (Wave::square_wave(Note::new(4, NoteName::C).pitch(), time) +
                  Wave::square_wave(Note::new(4, NoteName::E).pitch(), time) +
