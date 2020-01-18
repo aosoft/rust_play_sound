61b3c56ef6b6a53c46c99908e1159c41c1169d75
aosoft <tan-y@aosoft.jp>
2020-01-18 10:52:58 +0900

    Update sound functions

--- src/lib.rs
+++ src/lib.rs
@@ -22,10 +22,13 @@ pub struct Note {
 }
 
 const BASE_PITCH: f32 = 440.0;
-const BASE_NOTE: Note = Note { octave:4, note:NoteName::A };
+const BASE_NOTE: Note = Note { octave: 4, note: NoteName::A };
 
 #[derive(Copy, Clone)]
 pub struct Time(f32);
+
+#[derive(Copy, Clone)]
+pub struct Pitch(f32);
 
 #[derive(Copy, Clone)]
 pub struct Wave(f32);
@@ -39,37 +42,37 @@ impl Note {
         self.octave * 12 + self.note as i32
     }
 
-    pub fn pitch(&self) -> f32 {
-        BASE_PITCH * 2.0_f32.powf(((self.note_index() - BASE_NOTE.note_index()) as f32) / 12.0)
+    pub fn pitch(&self) -> Pitch {
+        Pitch(BASE_PITCH * 2.0_f32.powf(((self.note_index() - BASE_NOTE.note_index()) as f32) / 12.0))
     }
 }
 
 
-
 impl Time {
-    pub fn samples_to_time(sample_rate: f32, samples: i32) -> Time {
-        Time(samples as f32 / sample_rate)
+    pub fn samples_to_time(sample_rate: u32, samples: u32) -> Time {
+        Time(samples as f32 / sample_rate as f32)
     }
 
-    pub fn per_cycle(self, pitch:f32) -> f32 {
-        let x = self.0 * pitch;
+    pub fn per_cycle(self, pitch: Pitch) -> f32 {
+        let x = self.0 * pitch.0;
         x - x.floor()
     }
+}
 
+impl Pitch {
+    pub fn sine_wave(self, time: Time) -> Wave {
+        Wave((2.0 * PI * time.per_cycle(self)).sin())
+    }
+
+    pub fn square_wave(self, time: Time) -> Wave {
+        Wave(1.0 - time.per_cycle(self).round() * 2.0)
+    }
 }
 
 impl Wave {
     pub fn to_value(self) -> f32 { self.0 }
 
-    pub fn volume(self, vol:f32) -> Wave { Wave(self.0 * vol) }
-
-    pub fn sine_wave(pitch:f32, time:Time) -> Wave{
-        Wave((2.0 * PI * time.per_cycle(pitch)).sin())
-    }
-
-    pub fn square_wave(pitch:f32, time:Time) -> Wave {
-        Wave(1.0 - time.per_cycle(pitch).round() * 2.0)
-    }
+    pub fn volume(self, vol: f32) -> Wave { Wave(self.0 * vol) }
 }
 
 impl std::ops::Add for Wave {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use portaudio::{PortAudio, Error};
-use rust_play_sound::{ Note, NoteName, Wave, Time };
+use rust_play_sound::{ Note, NoteName, Time };
 
 const SAMPLE_RATE: u32 = 48000;
 
@@ -15,15 +15,15 @@ fn main2() -> Result<(), Error> {
     let mut settings = pa.default_output_stream_settings(2, SAMPLE_RATE as f64, SAMPLE_RATE)?;
     settings.flags = portaudio::stream_flags::CLIP_OFF;
 
-    let mut sample_number:i32 = 0;
+    let mut sample_number:u32 = 0;
     let callback = move |args: portaudio::OutputStreamCallbackArgs<f32>| {
         let mut i = 0;
         for _ in 0..args.frames {
-            let time = Time::samples_to_time(SAMPLE_RATE as f32,sample_number);
+            let time = Time::samples_to_time(SAMPLE_RATE,sample_number);
             let value =
-                (Wave::square_wave(Note::new(4, NoteName::C).pitch(), time) +
-                 Wave::square_wave(Note::new(4, NoteName::E).pitch(), time) +
-                 Wave::square_wave(Note::new(4, NoteName::G).pitch(), time)).volume(0.3);
+                (Note::new(4, NoteName::C).pitch().square_wave(time) +
+                 Note::new(4, NoteName::E).pitch().square_wave(time) +
+                 Note::new(4, NoteName::G).pitch().square_wave(time)).volume(0.3);
             args.buffer[i] = value.to_value();
             args.buffer[i + 1] = value.to_value();
 
